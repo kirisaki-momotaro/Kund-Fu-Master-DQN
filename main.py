4f57a7cb6ec31059dcbc083d196f8f498d06759c
christos ioannidis <74102218+kirisaki-momotaro@users.noreply.github.com>
2023-12-31 20:37:56 +0200

    created CNN

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 environment = DQNBreakout(device=device , render_mode='human')
 
 state = environment.reset()
-
+num_actions = environment.action_space.n
+print(f"Number of actions: {num_actions}") #print number of availabel actions
 for _ in range(100):
     action = environment.action_space.sample()
     state, reward,done ,info = environment.step(action)
---
+++ model.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+
+class AtariNet(nn.module):
+    def __init__(self, nb_actions=14):
+        super(AtariNet, self).__init__()
+
+        self.relu = nn.ReLU()
+        #TODO check CNN theory
+        self.conv1 = nn.Conv2d(1,32, kernel_size=(8, 8), stride=(4, 4))
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=(4, 4), stride=(2, 2))
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1))
+
+        self.flatten = nn.Flatten()
+        self.dropout = nn.Dropout(p=0.2) #drops some random weights to add some randomness
+
+        self.action_value1 = nn.Linear(3136, 1024)
+        self.action_value2 = nn.Linear(1024, 1024)
+        self.action_value3 = nn.Linear(1024, nb_actions)
+
+        self.state_value1 = nn.Linear(3136, 1024)
+        self.state_value2 = nn.Linear(1024, 1024)
+        self.state_value3 = nn.Linear(1024, 1)
+
+
+    def forward(self, x):
+        x =torch.Tensor(x)
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.relu(self.conv3(x))
+        x = self.flatten(x)
+
+        state_value = self.relu(self.state_value1(x))
+        state_value = self.dropout(state_value)
+        state_value = self.relu(self.state_value2(state_value))
+        state_value = self.dropout(state_value)
+        state_value = self.relu(self.state_value3(state_value))
+
+
+
+        action_value = self.relu(self.action_value1(x))
+        action_value = self.dropout(action_value)
+        action_value = self.relu(self.action_value2(action_value))
+        action_value = self.dropout(action_value)
+        action_value = self.action_value3(action_value)
+
+
+        output = state_value + (action_value - action_value.mean())
