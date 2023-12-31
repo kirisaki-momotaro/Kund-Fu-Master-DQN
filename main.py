b41f671cd7e89af28e4effa7bba6556f7e360038
christos ioannidis <74102218+kirisaki-momotaro@users.noreply.github.com>
2023-12-31 17:15:22 +0200

    added report

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import torch
+import gym
+import numpy as np
+from PIL import Image
+import os
 
-print(torch.rand(2,2))
+print(torch.rand(2, 2))
+print(torch.cuda.is_available())
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+environment = DQNBreakout
